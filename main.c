8e2c16958d24ff0c3266de5150e16507c2b1c08c
Rui Salgado <ruivfsalgado@gmail.com>
2013-01-01 03:10:27 +0000

    Fix build for main.c on c99

--- main.c
+++ main.c
@@ -20,51 +20,51 @@
 
 // Estruturas e enumeracoes subjacentes a aluno e instrutor
 
-typedef struct {
+typedef struct cod_postal {
     unsigned short int nr_local, nr_rua;
 } cod_postal;
 
-typedef struct {
+typedef struct morada {
     char rua[TAM];
     unsigned short int numero;
     cod_postal cod_postal;
     char localidade[SIZE];
-} morad;
+} morada;
 
-typedef struct {
+typedef struct telefone {
     unsigned short int ind_regional, numero1, numero2;
-} telefon;
+} telefone;
 
-typedef enum {
+typedef enum dia {
     UM, DOIS, TRES, QUATRO, CINCO, SEIS, SETE, OITO, NOVE, DEZ, ONZE,
     DOZE, TREZE, CATORZE, QUINZE, DEZASSEIS, DEZASSETE, DEZOITO, DEZANOVE, VINTE,
     VINTE_E_UM, VINTE_E_DOIS, VINTE_E_TRES, VINTE_E_QUATRO, VINTE_E_CINCO,
     VINTE_E_SEIS, VINTE_E_SETE, VINTE_E_OITO, VINTE_E_NOVE, TRINTA, TRINTA_E_UM
 } dia;
 
-typedef enum {
+typedef enum mes {
     JANEIRO, FEVEREIRO, MARCO, ABRIL, MAIO, JUNHO, JULHO, AGOSTO,
     SETEMBRO, OUTUBRO, NOVEMBRO, DEZEMBRO
 } mes;
 
-typedef struct {
+typedef struct data {
     dia dia;
     mes mes;
     unsigned short int ano;
 } data;
 
-typedef enum {
+typedef enum categoria {
     A, B, C, D
 } categoria;
 
-typedef enum {
+typedef enum rev_carta {
     Sim, Nao
 } rev_carta;
 
 
 // Estrutura para os dados do Aluno
 
-typedef struct {
+typedef struct aluno {
     unsigned long int ni;
     char nome[TAM];
     morada morada;
@@ -77,7 +77,7 @@ typedef struct {
 
 // Estrutura para os dados do Instrutor
 
-typedef struct {
+typedef struct instrutor {
     unsigned short int nr_licenca;
     char nome[TAM];
     morada morada;
@@ -87,14 +87,16 @@ typedef struct {
     unsigned short salario;
 } instrutor;
 
+void clear_input() {
+    char ch;
+    while ((ch = getchar()) != '\n' && ch != EOF);
+}
 
 // Funcao para inserir alunos
-
 void inserir_aluno(aluno alunos[], int nr_aluno) {
-
-
     puts("Inserir numero de identificacao: ");
     scanf("%lu", &alunos[nr_aluno].ni);
+    clear_input();
     puts("Escrever nome completo: ");
     fflush(stdin);
     fgets(alunos[nr_aluno].nome, TAM, stdin);
@@ -102,20 +104,25 @@ void inserir_aluno(aluno alunos[], int nr_aluno) {
     fflush(stdin);
     fgets(alunos[nr_aluno].morada.rua, TAM, stdin);
     puts("Inserir numera da porta: ");
-    scanf("%u", &alunos[nr_aluno].morada.numero);
+    scanf("%hu", &alunos[nr_aluno].morada.numero);
     puts("Inserir codigo postal: ");
-    scanf("%u - %u", &alunos[nr_aluno].morada.cod_postal.nr_local, &alunos[nr_aluno].morada.cod_postal.nr_rua);
+    scanf("%hu - %hu", &alunos[nr_aluno].morada.cod_postal.nr_local, &alunos[nr_aluno].morada.cod_postal.nr_rua);
     puts("Escrever a localidade onde habita: ");
     fflush(stdin);
     fgets(alunos[nr_aluno].morada.localidade, SIZE, stdin);
     puts("Inserir numero de telefone: ");
-    scanf("%u - %u - %u", &alunos[nr_aluno].telefone.ind_regional, &alunos[nr_aluno].telefone.numero1, &alunos[nr_aluno].telefone.numero2);
+    scanf("%hu - %hu - %hu", &alunos[nr_aluno].telefone.ind_regional, &alunos[nr_aluno].telefone.numero1, &alunos[nr_aluno].telefone.numero2);
     puts("Inserir data de nascimento (dia-mes-ano): ");
-    scanf("%d - %d - %u", &alunos[nr_aluno].data.dia, &alunos[nr_aluno].data.mes, &alunos[nr_aluno].data.ano);
+    int dia = (intptr_t)&alunos[nr_aluno].data.dia;
+    int mes = (intptr_t)&alunos[nr_aluno].data.mes;
+    int ano = (intptr_t)&alunos[nr_aluno].data.ano;
+    scanf("%d - %d - %d", &dia, &mes, &ano);
     puts("Introduzir a categoria de conducao na qual se esta a inscrever (A, B, C ou D): ");
-    scanf("%d", &alunos[nr_aluno].categoria);
+    int categoria = (intptr_t)&alunos[nr_aluno].categoria;
+    scanf("%d", &categoria);
     puts("Introduzir revalidacao de carta (sim ou nao): ");
-    scanf("%d", &alunos[nr_aluno].rev_carta);
+    int rev_carta = (intptr_t)&alunos[nr_aluno].rev_carta;
+    scanf("%d", &rev_carta);
 }
 
 void alterar_aluno(aluno alunos[], int nr_aluno) {
@@ -207,12 +214,12 @@ int main(void) {
 
 
     do {
-        system("cls");
+        system("clear");
         opcao = menu();
         switch (opcao) {
             case 1:
                 do {
-                    system("cls");
+                    system("clear");
                     opcao_aluno = menu_aluno();
                     switch (opcao_aluno) {
                         case 1:
@@ -221,7 +228,7 @@ int main(void) {
                                 inserir_aluno(alunos, nr_aluno);
                             }
 
-                            system("cls");
+                            system("clear");
                             break;
                         case 2:
 
@@ -230,7 +237,7 @@ int main(void) {
                             puts("Selecionada opcao 3 - Listar dados");
                             puts("Alunos inscritos:");
                             for (i = 0; i < CAP_MAX; i++) {
-                                printf("%d - %s", i, alunos);
+                                printf("%d - %s", i, alunos[i].nome);
                             }
 
                             break;
